5b16bb25002c936a82f56a419e2ad2b4a7582c54
marius.alba <kleemannmarius@gmail.com>
2017-07-03 15:03:10 +0200

    Updated the Comic Books Detail view layout

--- src/comicBooks/Models/ComicBooks.cs
+++ src/comicBooks/Models/ComicBooks.cs
@@ -27,7 +27,7 @@ namespace comicBooks.Models
         {
             get
             {
-                return SeriesTitle.Replace("", "-").ToLower() + "-" + IssueNumber + ".jpg";
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
             }
         }
     }
